1c54ef6937cd8b7754d3b66ac6dc261ad99c57c9
Ridho Aulia Rahman <220605110044@student.uin-malang.ac.id>
2024-02-18 17:23:13 +0700

    model contact

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contact extends Model
 {
-    use HasFactory;
+    protected $primarykey = "id";
+    protected $keyType = "int";
+    protected $table = "contacts";
+    protected $incrementing = true;
+    protected $timestamps = true;
+
+
+    public function user(): BelongsTo{
+        return $this->BelongsTo(Contact::class,"user_id","id");
+    }
 }
--- database/migrations/2024_02_18_071453_create_contacts_table.php
+++ database/migrations/2024_02_18_071453_create_contacts_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string("first_name",100)->nullable(false);
+            $table->string("last_name",100)->nullable();
+            $table->string("email",200)->nullable();
+            $table->string("phone",200)->nullable();
+            $table->unsignedBigInteger("user_id")->nullable(false);
             $table->timestamps();
+
+            // foreign key
+            $table->foreign("user_id")->on("users")->references("id");
+            
         });
     }
 
